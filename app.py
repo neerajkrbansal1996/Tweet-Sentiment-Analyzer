a7bb8a472c79d3487034e5b486a8535d16517cf6
Neeraj Bansal <neerajkrbansal1996@gmail.com>
2017-12-29 22:42:43 +0530

    main file changed
    main file changed

--- app.py
+++ app.py
@@ -17,5 +17,5 @@ def tweets():
         tweets = api.get_tweets()
         return jsonify({'data': tweets, 'count': len(tweets)})
 
-port = int(os.environ.get('PORT', 5000))
-app.run(host="0.0.0.0", port=port, debug=True)
+if __name__ == "__main__":
+	app.run()		
